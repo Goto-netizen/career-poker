e908a94194e4a72138bd37fbf0215b4e7c4bdcb0
emtech-user <emtech-user@192.168.13.114>
2022-05-27 10:05:39 +0900

    cardクラスをitemに

---
+++ Career/src/main/java/model/item/Card.java
@@ -0,0 +1,77 @@
+package model.item;
+
+import java.io.Serializable;
+
+public class Card implements Serializable{
+	
+	/**
+	 * カードID
+	 */
+	private int code;
+	
+	/**
+	 * 数字
+	 */
+	private String num;
+
+	/**
+	 * マーク
+	 */
+	private String mark;
+	
+	/**
+	 * 強さ
+	 */
+	private int strength;
+
+
+
+	/**
+	 * EmployeeBeanを構築します。
+	 */
+	public Card() {
+
+	}
+
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code=code;
+	}
+	/**
+	 * フィールドnumの値を返します。
+	 * @return 数字
+	 */
+	public String getNum() {
+		return num;
+	}
+
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+
+	/**
+	 * フィールドmarkの値を返します。
+	 * @return マーク
+	 */
+	public String getMark() {
+		return mark;
+	}
+
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	/**
+	 * 強さ
+	 * @return
+	 */
+	public int getStrength() {
+		return strength;
+	}
+	public void setStrength(int strength) {
+		this.strength=strength;
+	}
+}
