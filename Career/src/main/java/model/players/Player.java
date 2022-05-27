ce548e3921e6ed16af4aed4290836ed4263cd3d2
emtech-user <emtech-user@192.168.13.122>
2022-05-27 11:45:11 +0900

    Player.java更新

--- Career/src/main/java/model/players/Player.java
+++ Career/src/main/java/model/players/Player.java
@@ -1,6 +1,42 @@
 package model.players;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.item.Card;
+
 public class Player {
 	
+
+	/*
+	 *フィールド
+	 */
+	List<Card> playerHandList;
 	
+	/*
+	 * コンストラクタ
+	 */
+	public Player(List<Card>playerHandList) {
+		this.playerHandList = playerHandList;
+	}
+	
+	
+	public List<Card> PlayerProcess(int selectedIndex)throws Exception {
+		Card playerhand = playerHandList.get(selectedIndex);
+		
+		/*
+		 * fieldList保留　テスト用
+		 */
+		List<Card>fieldList = new ArrayList();
+		fieldList.add(playerhand);
+		
+		playerHandList.remove(selectedIndex);
+
+		return playerHandList;
+		
+	}
+	
+	
+
+
 }
