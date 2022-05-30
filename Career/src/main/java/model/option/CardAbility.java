30f14917b087430418b0f1afb7539d310d909ce8
emtech-user <emtech-user@192.168.13.114>
2022-05-30 11:35:31 +0900

    tenAbility

--- Career/src/main/java/model/option/CardAbility.java
+++ Career/src/main/java/model/option/CardAbility.java
@@ -1,5 +1,9 @@
 package model.option;
 
+import java.util.List;
+
+import model.item.Card;
+
 public class CardAbility {
 
 	
@@ -19,8 +23,11 @@ public class CardAbility {
 		
 	}
 	
-	public void tenAbility() {
+	public List<Card> tenAbility(List<Card> deckList ,int index) {
 		
+		deckList.get(index).setCard_flag(4);
+		
+		return deckList;
 	}
 	public void jackAbility() {
 		
