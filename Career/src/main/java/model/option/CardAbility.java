41c2d026d79540fd0bd88193f34d6983577fe56e
emtech-user <emtech-user@192.168.20.26>
2022-05-30 10:09:35 +0900

    カード効果　クラス作成

---
+++ Career/src/main/java/model/option/CardAbility.java
@@ -0,0 +1,36 @@
+package model.option;
+
+public class CardAbility {
+
+	
+	
+	CardAbility(){
+		
+	}
+	
+	public void sevenAbility() {
+		
+	}
+	
+	public void eightAbility() {
+		
+	}
+	
+	public void nineAbility() {
+		
+	}
+	
+	public void tenAbility() {
+		
+	}
+	public void jackAbility() {
+		
+	}
+	public void queenAbility() {
+		
+	}
+	public void kingAbility() {
+		
+	}
+	
+}
